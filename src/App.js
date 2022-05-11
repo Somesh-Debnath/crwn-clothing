f8fa9a5a2639adbd4fcc1227139c45dc0f693e50
Somesh Debnath <91829500+Somesh-Debnath@users.noreply.github.com>
2022-05-11 20:19:50 +0530

    Added Header Component and Routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes,Route} from "react-router-dom";
 import Homepage from './pages/Homepage/Homepage.components'
+import ShopPage from './pages/shop/shopage.component'
+import './App.css'
+import Header from './components/header/header.component';
 export default function App() {
   return (
+    <Router>
     <div>
-      <Homepage />
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/shop" element={<ShopPage />} /> 
+      </Routes>
     </div>
+    </Router>
   )
 }
